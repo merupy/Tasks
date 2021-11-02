f981b4724666798a7335ea0ea2c860f145af7cb5
merupy <49117459+merupy@users.noreply.github.com>
2021-11-02 16:55:31 +0545

    BinarySearch

--- BinarySearch/Program.cs
+++ BinarySearch/Program.cs
@@ -4,9 +4,44 @@ namespace BinarySearch
 {
     class Program
     {
-        static void Main(string[] args)
+        // Returns index of x if it is present in arr[],
+        // else return -1
+        static int binarySearch(String[] arr, String x)
         {
-            Console.WriteLine("Hello World!");
+            int l = 0, r = arr.Length - 1;
+            while (l <= r)
+            {
+                int m = l + (r - l) / 2;  // 0+(2-1)/2 = 
+
+                int res = x.CompareTo(arr[m]);
+
+                // Check if x is present at mid
+                if (res == 0)
+                    return m;
+
+                // If x greater, ignore left half
+                if (res > 0)
+                    l = m + 1;
+
+                // If x is smaller, ignore right half
+                else
+                    r = m - 1;
+            }
+
+            return -1;
+        }
+
+        // Driver method to test above
+        public static void Main(String[] args)
+        {
+            String[] arr = { "Meru", "Ram", "Shyam", "Hari" };
+            String x = "Ram";
+            int result = binarySearch(arr, x);
+
+            if (result == -1)
+                Console.WriteLine("Element not present");
+            else
+                Console.WriteLine("Element found at " + "index " + result);
         }
     }
 }
